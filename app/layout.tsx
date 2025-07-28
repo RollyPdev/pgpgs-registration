19c86bb5b82866d1b5ee702977160665e68cb418
Rolly Ortiz Paredes <coeusrolly@gmail.com>
2025-07-28 16:31:49 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,8 +88,9 @@ export default function RootLayout({
         <meta name="author" content="PGPGS Organization" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="apple-mobile-web-app-title" content="PGPGS Registration" />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico?v=2" sizes="any" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" />
         <link rel="icon" type="image/png" sizes="32x32" href="/PGPGS Logo.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/PGPGS Logo.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/PGPGS Logo.png" />
